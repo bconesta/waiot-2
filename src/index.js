dd6df083f396887492e27480d68c3a803c4bed57
Bruno Conesta <bconesta@gmail.com>
2023-03-02 14:32:42 -0300

    v0.9

--- src/index.js
+++ src/index.js
@@ -21,5 +21,5 @@ root.render(
     <App />
 );
 
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
--- src/tabs/Settings.jsx
+++ src/tabs/Settings.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { BsGlobe2, BsClock, BsPerson, BsInfoCircle } from 'react-icons/bs';
 
 export default function Settings() {
   return (
-    <div>Settings</div>
+    <div className='Settings'>
+      <div className='item'>
+        <BsGlobe2 />
+        <h3>Lenguaje</h3>
+        <select name='' disabled>
+          <option value='es'>Español</option>
+          <option value='en'>English</option>
+        </select>
+      </div>
+      <div className='item'>
+        <BsClock />
+        <h3>Formato de hora</h3>
+        <select name='' disabled>
+          <option value='24'>24</option>
+          <option value='12' disabled>12</option>
+        </select>
+      </div>
+      <div className='item'>
+        <BsPerson />
+        <h3>Cuenta</h3>
+      </div>
+      <div className='item'>
+        <BsInfoCircle />
+        <h3>Sobre la app</h3>
+      </div>
+      
+    </div>
   )
 }
